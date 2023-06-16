0ee32acb73d17122f378bddddf36cc865c47ebee
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-06-16 16:49:18 +0800

    Basic Utility - Bagian 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      nunito: ['Nunito Sans', 'sans-serif'],
+    },
+    // colors: {
+    //   ags: '#00ff00',
+    // },
   },
   plugins: [],
 }
