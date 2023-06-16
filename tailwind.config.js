7da6a911bc2ebcf7a2b8b944e28c75f904f72b17
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-06-16 17:04:59 +0800

    configure custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,19 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#5c6ac4',
+        secondary: '#ecc94b',
+      }
+    },
     fontFamily: {
       nunito: ['Nunito Sans', 'sans-serif'],
     },
     // colors: {
     //   ags: '#00ff00',
     // },
+    
   },
   plugins: [],
 }
